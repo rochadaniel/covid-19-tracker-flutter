f3042faa111d19074729503fa4696401ab009533
Daniel Rocha <daniel.rocha@pebmed.com.br>
2020-04-05 23:47:20 -0300

    Creating screens structure

---
+++ lib/presentation/main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
