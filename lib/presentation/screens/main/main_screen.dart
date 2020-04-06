bcf4a3b97d54b4672d0dd71aad6f1e8826f63ba8
Daniel Rocha <daniel.rocha@pebmed.com.br>
2020-04-06 00:15:49 -0300

    Adding main screen bottom bar

--- lib/presentation/screens/main/main_screen.dart
+++ lib/presentation/screens/main/main_screen.dart
@@ -1,13 +1,51 @@
+import 'package:covid19app/presentation/screens/main/pages/home_page.dart';
+import 'package:covid19app/presentation/screens/main/pages/world_page.dart';
+import 'package:covid19app/utils/constants.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:line_icons/line_icons.dart';
 
+/// References
+/// Save Page state: https://stackoverflow.com/a/55512883
 class MainScreen extends StatefulWidget {
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final List<Widget> _pages = [HomePage(), WorldPage()];
+
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Covid-19"),
+        backgroundColor: Constants.backgroundColor,
+      ),
+      body: Container(
+          color: Constants.foregroundColor,
+          child: IndexedStack(
+            children: _pages,
+            index: _selectedIndex,
+          )
+      ),
+      bottomNavigationBar: _bottomNavigationBar(_selectedIndex),
+    );
   }
+
+  Widget _bottomNavigationBar(int selectedIndex) => BottomNavigationBar(
+    onTap: (int index) => setState(() => _selectedIndex = index),
+    currentIndex: _selectedIndex,
+    backgroundColor: Constants.backgroundColor,
+    selectedItemColor: Colors.white,
+    unselectedItemColor: Colors.grey[500],
+    items: [
+      BottomNavigationBarItem(
+          icon: Icon(LineIcons.home), title: Text('Home')),
+      BottomNavigationBarItem(
+          icon: Icon(LineIcons.globe), title: Text('World')),
+    ],
+  );
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
 class Constants {
   //API
   static const String HEROKU_ENDPOINT = "https://coronavirus-19-api.herokuapp.com/";
+
+  //Colors
+  static const Color backgroundColor = Color(0xff272936);
+  static const Color foregroundColor = Color(0xff3d3f50);
 }
