ec37b6eda4e0fd521a37e176f2b311c280c75d52
Daniel Rocha <rocha.daniel52@gmail.com>
2020-04-11 01:19:20 -0300

    Add search component. Fixes #1

--- lib/presentation/screens/main/pages/search_page.dart
+++ lib/presentation/screens/main/pages/search_page.dart
@@ -3,6 +3,7 @@ import 'package:covid19app/presentation/screens/country/country_details_screen.d
 import 'package:covid19app/utils/constants.dart';
 import 'package:covid19app/utils/number_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,6 +16,27 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<CountryCoronaModel> items;
+  TextEditingController controller = new TextEditingController();
+  String filter;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(() {
+      setState(() {
+        filter = controller.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -50,39 +72,74 @@ class _SearchPageState extends State<SearchPage> {
       padding: EdgeInsets.all(8.0),
       itemCount: countries.length,
       itemBuilder: (BuildContext context, int index) {
-        final countryCoronaItem = countries[index];
-        return Card(
-          color: Constants.backgroundColor,
-          child: ListTile(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CountryDetailsScreen(
-                      countryCoronaModel: countryCoronaItem),
-                ),
-              );
-            },
-            leading: CircleAvatar(
-              radius: 30.0,
-              backgroundImage:
-              NetworkImage(countryCoronaItem.countryInfo.flag),
-              backgroundColor: Colors.transparent,
-            ),
-            title: Text(
-              countryCoronaItem.country,
+        if (index == 0) {
+          return Container(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              cursorColor: Colors.white,
               style: TextStyle(color: Colors.white),
+              decoration: new InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  labelText: "Search",
+                  labelStyle: TextStyle(color: Colors.grey[200]),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 1,color: Colors.white, style: BorderStyle.solid)
+                ),
+              ),
+              controller: controller,
             ),
-            subtitle: Text(
-              "${Constants.TOTAL_CASES_STRING}: ${NumberUtils.formatDecimalPlaces(countryCoronaItem.cases)}",
-              style: TextStyle(color: Colors.grey[300]),
-            ),
-            trailing: Icon(
-              Icons.arrow_forward,
-              color: Colors.white,
-            ),
-          ),
-        );
+          );
+        } else {
+          final countryCoronaItem = countries[index - 1];
+          if (filter == null ||
+              filter == "" ||
+              countryCoronaItem.country
+                  .toLowerCase()
+                  .contains(filter.toLowerCase())) {
+            return Card(
+              color: Constants.backgroundColor,
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CountryDetailsScreen(
+                          countryCoronaModel: countryCoronaItem),
+                    ),
+                  );
+                },
+                leading: Image.network(
+                  countryCoronaItem.countryInfo.flag,
+                  width: 60,
+                  height: 60,
+                ),
+//            leading: CircleAvatar(
+//              radius: 30.0,
+//              backgroundImage:
+//              NetworkImage(countryCoronaItem.countryInfo.flag),
+//              backgroundColor: Colors.transparent,
+//            ),
+                title: Text(
+                  countryCoronaItem.country,
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  "${Constants.TOTAL_CASES_STRING}: ${NumberUtils.formatDecimalPlaces(countryCoronaItem.cases)}",
+                  style: TextStyle(color: Colors.grey[300]),
+                ),
+                trailing: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                ),
+              ),
+            );
+          } else {
+            return Container();
+          }
+        }
       },
     );
   }
